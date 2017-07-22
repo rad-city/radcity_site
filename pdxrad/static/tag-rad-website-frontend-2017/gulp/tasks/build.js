ba259fc10d9f610f7b189a6678002dd972e9233f
Braden Jageman <bjageman@gmail.com>
2017-07-21 19:28:27 -0700

    some updates to the template and a dynamic menu added

--- pdxrad/static/tag-rad-website-frontend-2017/gulp/tasks/build.js
+++
@@ -1,5 +0,0 @@
-import gulp from 'gulp';
-import plugins from '../utils/plugins';
-
-// In NPM it is setup with: gulp clean && gulp build --production
-gulp.task('build', plugins.sequence('clean', ['style', 'js', 'img', 'html', 'copy']));
--- pdxrad/static/tag-rad-website-frontend-2017/src/script/utils/log.js
+++
@@ -1,6 +0,0 @@
-import Captain from 'captainslog';
-
-const cptn = new Captain('Laust Deleuran');
-cptn.toggleDebug(true);
-
-export default cptn;
