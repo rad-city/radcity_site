a04e7174c65a4138788eea2bca65bca3c2c5c6a2
Braden Jageman <bjageman@gmail.com>
2017-07-22 10:50:44 -0700

    trying to added required reading custom plugins but it's complicated

--- pdxrad/cms_plugins.py
+++ pdxrad/cms_plugins.py
@@ -3,7 +3,7 @@ from cms.plugin_pool import plugin_pool
 from cms.models.pluginmodel import CMSPlugin
 from django.utils.translation import ugettext_lazy as _
 
-from .models import Feature
+from .models import Feature, Article
 
 class FeaturePlugin(CMSPluginBase):
     model = Feature
@@ -15,4 +15,15 @@ class FeaturePlugin(CMSPluginBase):
         context = super(FeaturePlugin, self).render(context, instance, placeholder)
         return context
 
+class ArticlePlugin(CMSPluginBase):
+    model = Article
+    name = _("RAD Article Plugin")
+    render_template = "plugins/required-reading/section/article.html"
+    cache = False
+
+    def render(self, context, instance, placeholder):
+        context = super(ArticlePlugin, self).render(context, instance, placeholder)
+        return context
+
 plugin_pool.register_plugin(FeaturePlugin)
+plugin_pool.register_plugin(ArticlePlugin)
--- pdxrad/models.py
+++ pdxrad/models.py
@@ -7,3 +7,13 @@ class Feature(CMSPlugin):
     blurb = models.CharField(max_length=256, default='Just a short blurb here.')
     img = models.CharField(max_length=256)
     link = models.CharField(max_length=256)
+
+class Article(CMSPlugin):
+    title = models.CharField( max_length=64, default='Article Title')
+    description = models.CharField(max_length=2056, default='Just a short blurb here.')
+    link = models.CharField(max_length=256)
+
+class ReadingDivider(CMSPlugin):
+    title = models.CharField( max_length=64, default='Article Title')
+    description = models.CharField(max_length=2056, default='Just a short blurb here.')
+    link = models.CharField(max_length=256)
